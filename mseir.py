4dac2897bb9959f2c9880d6aae55707f36dce769
Quentin CAUDRON <quentincaudron@gmail.com>
2014-07-28 14:59:33 -0400

    Still testing inference

--- mseir.py
+++ mseir.py
@@ -6,12 +6,12 @@ import seaborn
 
 N = 2400000.
 mu = 1./21900.
-births = 137.
-r0 = 20.
-gamma = 1./12. # recovery rate
+births = 138.
+r0 = 17.2
+gamma = 1./14. # recovery rate
 
-L = 3650
-dt = 0.05
+L = 156500
+dt = 0.2
 T = np.linspace(0, L/365., L/dt)
 
 S = np.arange(0, L, dt)
@@ -19,22 +19,22 @@ I = np.arange(0, L, dt)
 R = np.arange(0, L, dt)
 
 S[0] = 168000.
-I[0] = 24.
+I[0] = 8.
 R[0] = N - S[0] - I[0]
 
 
 
 for t in range(1, len(S)) :
 
-	S[t] = S[t-1] + dt * (births - r0 * gamma * S[t-1] * I[t-1] / N - mu * S[t-1])
-	I[t] = I[t-1] + dt * (r0 * gamma * S[t-1] * I[t-1] / N - gamma * I[t-1] - mu * I[t-1])
-	R[t] = R[t-1] + dt * (gamma * I[t-1] - mu * R[t-1])
+	S[t] = S[t-1] + dt * (births - r0 * (1. + np.sin(2*np.pi*t*dt/365.)/10.) * gamma * S[t-1] * I[t-1] / N - mu * S[t-1])
+	I[t] = I[t-1] + dt * (r0 * (1. + np.sin(2*np.pi*t*dt/365.)/10.) * gamma * S[t-1] * I[t-1] / N - gamma * I[t-1] - mu * I[t-1])
+	#R[t] = R[t-1] + dt * (gamma * I[t-1] - mu * R[t-1])
 	
 
+R = N - S - I
 
 
-
-plt.plot(T, S+I+R)
+plt.plot(T, I)
 plt.show()
 
 
